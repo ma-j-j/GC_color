39a7a6ae8da5c026412566915ef33fdf2ecef9bb
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-03-31 19:50:53 +0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,271 +1,91 @@
-#include"标头.h"
-using namespace cv;
-using namespace std;
+#include "OfflineTest.h"
+#include <QListView>
 
-bool  ChipDetect_algo::chipNG_algo(Rect detectroi,Mat binaryimg,int i)
+
+OffLineTest::OffLineTest(Ui::MainWindowView *ui1,QObject *parent)
+    : QObject{parent}, ui(ui1)
 {
-	Mat stats, centroids;
-	vector<float>areas;
-	Mat labels = Mat::zeros(binaryimg.size(), CV_32S);
-	Mat temp1;
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
-	morphologyEx(binaryimg, temp1, MORPH_OPEN, kernel);
-	int labalsnum = connectedComponentsWithStats(~temp1(detectroi), labels, stats, centroids, 8, CV_32S, CCL_DEFAULT);
-//	imshow(to_string(i), ~temp1(detectroi));
-	int num = 0;
-	for (int j = 0; j < labalsnum; j++)
-	{
 
-		if (stats.at<int>(j, CC_STAT_AREA) > 16)
-		{
-			areas.push_back((float)stats.at<int>(j, CC_STAT_AREA));
-			num++;
-			//cout << stats.at<int>(j, CC_STAT_AREA) << endl;
-		}
-	}
-	
-	float sd = this->calculateStandardDeviation(areas);
-	cout << "连通个数：" << num << endl;
-	cout << "方差：" << sd << endl;
-	if (sd > 4000 || num < 25)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    ui->comboBox_IC->addItem(u8"11.5x13");
+    ui->comboBox_IC->addItem(u8"11x13");
+    ui->comboBox_IC->setCurrentIndex(0);
+    QAbstractItemView *abstractViewIC = ui->comboBox_IC->view();
+    QListView *listViewIC = qobject_cast<QListView *>(abstractViewIC);
+    listViewIC->setSpacing(6);
+
+    // 连接信号与槽
+    connect(ui->pushButton_OpenImg_2, &QPushButton::clicked, this, &OffLineTest::slotOffLineTestingView_On_PushButton_OpenImg_Clicked);//打开图片
+    connect(ui->pushButton_StartTest,&QPushButton::clicked,this,&OffLineTest::slotOffLineTestingView_On_PushButton_StartTest_Clicked);//开始测试
+    changeStyle();
+}
+
+void OffLineTest::setImg(QImage qImage)
+{
+    if(true == qImage.isNull())
+    {
+        qDebug()<<"OffLineTestingView::setImg 传入图像为NULL";
+        return;
+    }
+    ui->graphicsView_Test->loadQPixmapImage(QPixmap::fromImage(qImage));
+    this->ui->graphicsView->setVisible(false);
+    this->ui->graphicsViewAnno->setVisible(false);
+    this->ui->graphicsView_Test->setVisible(true);
+    ui->label_ShowImage->setText("离线测试图片");
+}
+
+
+//选择图片
+void OffLineTest::slotOffLineTestingView_On_PushButton_OpenImg_Clicked()
+{
+    QString file_path = QFileDialog::getOpenFileName(nullptr,tr("Open Image"),"15000_IMG",tr("Image Files(*.jpg *.bmp *.png)"));
+    if(true == file_path.isEmpty())
+    {
+        // 取消了对话框
+        qDebug()<<u8"取消选择图片";
+        return ;
+    }
+    this->ui->graphicsView->setVisible(true);//显示原图
+    this->ui->graphicsViewAnno->setVisible(false);
+    this->ui->graphicsView_Test->setVisible(true);
+    ui->label_ShowImage->setText("离线测试图片");
+
+    // 将图像路径显示到界面的lineEdit
+    ui->lineEdit_filePath_2->setText(file_path);
 
 }
 
-void ChipDetect_algo::detecting(Mat& img, int binarynum )
+//开始测试
+void OffLineTest::slotOffLineTestingView_On_PushButton_StartTest_Clicked()
 {
-	cout << "1" << endl;
-	namedWindow("frame", cv::WINDOW_NORMAL);
-	Mat gray, binaryimg;
-	//GaussianBlur(img, img, Size(3, 3), 0);
-	// 新增输入验证
-	if (img.empty() || img.channels() != 3) {
-		cerr << "Invalid input image! (空图像或通道数错误)"
-			<< " Channels: " << img.channels()
-			<< " Size: " << img.size() << endl;
-		cout << "2" << endl;
-	}
-	cout << "3" << endl;
+    // 将界面设置的信息汇总发送给后端进行检测
+    // 获取图像路径
+    QString imgFile = ui->lineEdit_filePath_2->text();
+    if(true == imgFile.isEmpty())
+    {
+        // 图像路径为空
+        QMessageBox::warning(NULL,u8"Error",u8"图像路径为空");
+        return;
+    }
 
-	//cvtColor(img, gray, COLOR_BGR2GRAY);
-	cout << "4" << endl;
+    // 获取芯片类型
+    IC_Type type = IC_Type::A;
+    QString strType = ui->comboBox_IC->currentText();
+    if("11.5x13" == strType)
+    {
+        type = IC_Type::A;
+    }
+    else if("11x13" == strType)
+    {
+        type = IC_Type::B;
+    }
 
-	threshold(img, binaryimg, binarynum, 255, THRESH_BINARY_INV);
-	/*Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5), Point(-1, -1));
-	morphologyEx(binaryimg, binaryimg,MORPH_CLOSE,kernel);*/
+    // 将信息发送到后端进行检测
+    emit this->signalOffLineTestingView_Send_Detection(imgFile,type);
 
-	//Mat temp;
-	//resize(binaryimg, temp, Size(binaryimg.cols / 4, binaryimg.rows / 4));
-	//imshow("testb", temp);
-	//imwrite("D:\code\Daheng_code/3.bmp", binaryimg);
+    //检测测试
+    rect_detect img_detect;
+    cv::Mat result;
+    // img_detect.calibration();
+    img_detect.ROI(imgFile,150,result);
 
-	vector<vector<Point>>contours;
-	findContours(binaryimg, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-	Rect roi; vector<Rect>rois;
-	for (int i = 0; i < contours.size(); i++)
-	{
-		roi = boundingRect(contours[i]);
-		float rate = (float)roi.width / roi.height;
-		if (rate > 2.2 || rate < 1.1||roi.x<img.cols*0.25|| roi.x > img.cols * 0.75||roi.area()<900000||roi.area()>1200000)continue;
-		rois.push_back(roi);
-	}
-	for (int i = 0; i < rois.size(); i++)
-	{
-		rectangle(img, rois[i], Scalar(0, 255, 255), 4);
-	}
-	
-	bool chipNG = false;
-	
-	for (int i = 0; i < rois.size(); i++)
-	{
-		cout << rois[i].x << "," << rois[i].y << endl;
-		int x = rois[i].x + rois[i].width * 0.35;
-		int y = rois[i].y + rois[i].height * 0.36;
-
-		Rect detectroi = Rect(Point(x, y), Size(rois[i].width * 0.3, rois[i].height * 0.28));
-		rectangle(img, detectroi, Scalar(0, 0, 255), 4, 8);
-		chipNG = this->chipNG_algo(detectroi, binaryimg, i);
-		Rect detectroi_left; bool leftNG = true;
-		for (int j = detectroi.width; j < binaryimg.cols * 0.25; j += detectroi.width)
-		{
-			detectroi_left = Rect(Point(j, detectroi.y), detectroi.size());
-			cout << "debug" << endl;
-			leftNG = this->chipNG_algo(detectroi_left, binaryimg, i);
-			if (leftNG == false)
-			{
-				rectangle(img, detectroi_left, Scalar(0, 0, 255), 4);
-				break;
-			}
-		}
-		Rect detectroi_right; bool rightNG = true;
-		for (int k = binaryimg.cols - detectroi.width * 2; k > binaryimg.cols * 0.7; k -= detectroi.width)
-		{
-			detectroi_right = Rect(Point(k, detectroi.y), detectroi.size());
-			cout << "debug" << endl;
-			rightNG = this->chipNG_algo(detectroi_right, binaryimg, i);
-			if (rightNG == false)
-			{
-				rectangle(img, detectroi_right, Scalar(0, 0, 255), 4);
-				break;
-			}
-		}
-
-		if (leftNG == false)
-		{
-			int startx = detectroi_left.x; int starty = detectroi_left.y - 25;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row > rois[i].y + 0.1 * rois[i].height; row--)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				leftNG = true;
-				cout << "upng" << endl;
-				cout << blackline << endl;
-			}
-		}
-		if (leftNG == false)
-		{
-			int startx = detectroi_left.x; int starty = detectroi_left.y - 25 + detectroi_left.height;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row < rois[i].y + 0.9 * rois[i].height; row++)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				leftNG = true;
-				cout << "downng" << endl;
-				cout << blackline << endl;
-			}
-		}
-
-
-		if (chipNG == false)
-		{
-			int startx = detectroi.x; int starty = detectroi.y - 25;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row > rois[i].y + 0.1 * rois[i].height; row--)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				chipNG = true;
-				cout << "upng" << endl;
-				cout << blackline << endl;
-			}
-		}
-		if (chipNG == false)
-		{
-			int startx = detectroi.x; int starty = detectroi.y - 25 + detectroi.height;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row < rois[i].y + 0.9 * rois[i].height; row++)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				chipNG = true;
-				cout << "downng" << endl;
-				cout << blackline << endl;
-			}
-		}
-
-
-		if (rightNG == false)
-		{
-			int startx = detectroi_right.x; int starty = detectroi_right.y - 25;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row > rois[i].y + 0.1 * rois[i].height; row--)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				rightNG = true;
-				cout << "upng" << endl;
-				cout << blackline << endl;
-			}
-
-		}
-		if (rightNG == false)
-		{
-			int startx = detectroi_right.x; int starty = detectroi.y - 25 + detectroi_right.height;
-			int blackline = 0;
-			for (int col = startx; col < startx + 0.5 * rois[i].width; col++)
-			{
-				int blackpts = 0; int pts = 0;
-				for (int row = starty; row < rois[i].y + 0.9 * rois[i].height; row++)
-				{
-					pts++;
-					int pv = binaryimg.at<uchar>(row, col);
-					if (pv == 0)blackpts++;
-				}
-				if ((float)blackpts / pts > 0.8)blackline++;
-			}
-			if (blackline < 5)
-			{
-				{
-					rightNG = true;
-					cout << "downng" << endl;
-					cout << blackline << endl;
-				}
-
-			}
-			
-		}
-		cout << to_string(i) << leftNG << endl;
-		cout << to_string(i) << chipNG << endl;
-		cout << to_string(i) << rightNG << endl;
-	}
-
-	cout << "5" << endl;
-
-	//resize(img, img, Size(img.cols / 4, img.rows / 4));
-	imshow("frame", img);
-	imwrite("D:\\code\\Daheng_code\\3.bmp", img);
-	waitKey(0);
 }
