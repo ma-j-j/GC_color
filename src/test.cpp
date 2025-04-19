b1ff01a71249a4840aab062c079f199a530ca3ec
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-04-19 13:37:50 +0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,40 +1,109 @@
-bool BaseCamera_Daheng::takePhoto(
-    const QVector< bool > &image_valid_array,
-    result_process_fun_t new_result_process_fun,
-    void *user_data ) noexcept {
-    auto data_ptr = m_d.data_ptr();
-    if( bool _Expected = false;
-        !m_d->m_under_callback_flag.compare_exchange_strong( _Expected, true ) ) {
-        return false;
+#include "daheng_camera.h"
+
+DaHeng_camera::DaHeng_camera(QObject *parent)
+{
+    this->m_Photo = new CSampleCaptureEventHandler();
+}
+CSampleCaptureEventHandler::CSampleCaptureEventHandler(QObject *parent)
+{
+    // connect(this,
+    //         &CSampleCaptureEventHandler::signals_TakePhoto,
+    //         this,
+    //         &CSampleCaptureEventHandler::slotProcess_Img); //传图片进行检测
+}
+
+void CSampleCaptureEventHandler::DoOnImageCaptured(CImageDataPointer &objImageDataPointer,
+                                                   void *pUserParam)
+{
+    cv::Mat image;
+    image.create(objImageDataPointer->GetHeight(), objImageDataPointer->GetWidth(), CV_8UC3);
+    void *pRGB24Buffer = NULL;
+    // 假设原始数据是BayerRG8图像
+    pRGB24Buffer = objImageDataPointer->ConvertToRGB24(GX_BIT_0_7, GX_RAW2RGB_NEIGHBOUR, true);
+    memcpy(image.data,
+           pRGB24Buffer,
+           (objImageDataPointer->GetHeight()) * (objImageDataPointer->GetWidth()) * 3);
+    img = image.clone();
+    if (!img.empty()) {
+        cv::imwrite("photo.png", img);
+        // emit signals_TakePhoto(img);
+        qDebug() << "拍照成功";
+    } else {
+        qDebug() << "拍照失败";
     }
-    auto &&guard1 = qScopeGuard( [ & ]() noexcept {
-        m_d->m_under_callback_flag.store( false, std::memory_order_release );
-    } );
-    if( !data_ptr->m_isCameraOpened ) {
-        data_ptr->m_error_process_fun( "Camera is not opened" );
-        return false;
+}
+
+cv::Mat &CSampleCaptureEventHandler::getImg()
+{
+    return img;
+}
+
+void DaHeng_camera::sent_TakePhoto()
+{
+    src = this->m_Photo->getImg();
+    if (!src.empty()) {
+        qDebug() << "src图像不为空";
+        emit this->signals_TakePhoto(src);
+    } else {
+        qDebug() << "src图像为空";
     }
-    else if( image_valid_array.size() != 2 ) {
-        data_ptr->m_error_process_fun( "InValid image_valid_array" );
-        return false;
+}
+
+// 接收图像进行处理
+void CSampleCaptureEventHandler::slotProcess_Img(cv::Mat img)
+{
+    if (!img.empty()) {
+        // cv::imwrite("photo.png", img);
+        qDebug() << "图像不为空";
     }
-    else if( data_ptr->m_mode.mode != EmExecuteMode::emTakePhoto ) {
-        data_ptr->m_error_process_fun( "InValid Camera Mode" );
-        return false;
+}
+
+// 初始化相机
+void DaHeng_camera::open_camera()
+{
+    ICaptureEventHandler *pCaptureEventHandler = NULL; ///<采集回调对象
+
+    //初始化
+    IGXFactory::GetInstance().Init();
+
+    gxdeviceinfo_vector vectorDeviceInfo;
+    IGXFactory::GetInstance().UpdateDeviceList(1000, vectorDeviceInfo);
+    if (0 == vectorDeviceInfo.size()) {
     }
-    data_ptr->m_takphoto_mode.m_result_process_fun = { new_result_process_fun, user_data };
-    data_ptr->m_takphoto_mode.m_image_valid_array = image_valid_array;
-    try {
-        data_ptr->m_objFeatureControlPtr->GetCommandFeature( "TriggerSoftware" )->Execute();
-    }
-    catch( std::exception &e ) {
-        data_ptr->m_error_process_fun( e.what() );
-        return false;
-    }
-    catch( ... ) {
-        data_ptr->m_error_process_fun( "unkown error" );
-        return false;
-    }
-    guard1.dismiss();
-    return true;
+    //打开第一台设备以及设备下面第一个流
+    CGXDevicePointer ObjDevicePtr
+        = IGXFactory::GetInstance().OpenDeviceBySN(vectorDeviceInfo[0].GetSN(), GX_ACCESS_EXCLUSIVE);
+    CGXStreamPointer ObjStreamPtr = ObjDevicePtr->OpenStream(0);
+
+    //获取远端设备属性控制器
+    this->m_ObjFeatureControlPtr = ObjDevicePtr->GetRemoteFeatureControl();
+    //注册远端设备事件:曝光结束事件【目前只有千兆网系列相机支持曝光结束事件】
+    //选择事件源
+    this->m_ObjFeatureControlPtr->GetEnumFeature("EventSelector")->SetValue("ExposureEnd");
+    //使能事件
+    this->m_ObjFeatureControlPtr->GetEnumFeature("EventNotification")->SetValue("On");
+
+    // 设置触发模式为On
+    this->m_ObjFeatureControlPtr->GetEnumFeature("TriggerMode")->SetValue("On");
+    // 设置触发源为软触发
+    this->m_ObjFeatureControlPtr->GetEnumFeature("TriggerSource")->SetValue("Software");
+
+    //注册回调采集
+    pCaptureEventHandler = new CSampleCaptureEventHandler();
+    ObjStreamPtr->RegisterCaptureCallback(pCaptureEventHandler, NULL);
+    //发送开采命令
+    ObjStreamPtr->StartGrab();
+    this->m_ObjFeatureControlPtr->GetCommandFeature("AcquisitionStart")->Execute();
+
+    qDebug() << "相机初始化成功";
+}
+
+void DaHeng_camera::slotTakePhoto()
+{
+    qDebug() << "收到拍照信号，开始拍照";
+    // 发送软触发命令
+    this->m_ObjFeatureControlPtr->GetCommandFeature("TriggerSoftware")->Execute();
+    // if (!this->m_Photo->img.empty()) {
+    sent_TakePhoto();
+    // }
 }
