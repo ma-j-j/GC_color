2dd906e52e218f01cd4dca24f684e89f71c695c1
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-04-23 11:35:13 +0800

    Update test.h

--- src/test.h
+++ src/test.h
@@ -1,45 +1,53 @@
-#pragma once
-#include<opencv2/opencv.hpp>
-#include<iostream>
-#include <vector>
-#include <cmath>
-#include <numeric>
-using namespace cv;
-using namespace std;
+#ifndef DAHENG_CAMERA_H
+#define DAHENG_CAMERA_H
 
-class ChipDetect_algo
+#include <GalaxyIncludes.h>
+
+#include <QDebug>
+#include <QObject>
+
+#include "opencv2/opencv.hpp"
+
+#include "QX-800_Model/Img_detect.h"
+
+// 用户继承采集事件处理类
+class CSampleCaptureEventHandler : public QObject, public ICaptureEventHandler
 {
-	double distance(Point pt1, Point pt2)
-	{
-		return pow(pow(pt1.x -pt2.x,2)+ pow(pt1.y +-pt2.y, 2), 0.5);
-	}
-    float calculateStandardDeviation( std::vector<float> data) 
-    {
-        if (data.empty()) {
-            return 0.0f;
-        }
-        sort(data.begin(), data.end());
-        data.pop_back();
-        swap(data[0], data[data.size()-1]);
-        data.pop_back();
-        float sum = std::accumulate(data.begin(), data.end(), 0.0f);
-        float mean = sum / data.size();
-        cout << "平均值：" << mean << endl;
-        float squared_diff_sum = 0.0f;
-        for (float num : data) {
-            float diff = num - mean;
-            squared_diff_sum += diff * diff;
-        }
-        float variance = squared_diff_sum / data.size();
-        if (mean < 900)
-        {
-            return  std::sqrt(variance) + 5000;
-        }
-        return std::sqrt(variance);
-    }
-    bool chipNG_algo(Rect detectroi,Mat binaryimg,int i);
-  
+    Q_OBJECT
+private:
 public:
-	void detecting(Mat& img, int binarynum);
+    cv::Mat img;
+    explicit CSampleCaptureEventHandler(QObject *parent = nullptr);
 
+    void DoOnImageCaptured(CImageDataPointer &objImageDataPointer,
+                           void *pUserParam); //回调函数采集图像
+signals:
+    void signals_TakePhoto(cv::Mat img); //发图片
+    // void signals_test(int);
 };
+
+class DaHeng_camera : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DaHeng_camera(QObject *parent = nullptr);
+    void open_camera(); //初始化相机
+    bool *getisSavePicutePtr() { return &isSavePicture; }            //获取是否保存图片的指针
+    void setSavePicture(bool isSave) { *isSavePicturePtr = isSave; } //更改是否保存图片的指针
+
+public slots:
+    void slotTakePhoto(); //软触发拍照
+    void slotSavePictuer(); //离线检测的保存图片按钮触发
+
+signals:
+    void signals_TakePhoto(cv::Mat img);  // 发图片用于算法检测
+    void signal_SavePicture(cv::Mat img); // 发图片用于保存
+
+private:
+    CGXFeatureControlPointer m_ObjFeatureControlPtr;
+    CSampleCaptureEventHandler *m_Photo;
+    bool isSavePicture;                      //是否保存图片;
+    bool *isSavePicturePtr = &isSavePicture; //是否保存图片的指针
+};
+
+#endif // DAHENG_CAMERA_H
