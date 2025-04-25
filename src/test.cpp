530d3d6cd1b2c6b675cd9b63cee82265ba3b9196
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-04-25 15:25:13 +0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,42 +1,33 @@
-#ifndef RECEIVE_SIGNAL_H
-#define RECEIVE_SIGNAL_H
-#include <iostream>
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
 
-#include <QDebug>
-#include <QFile>
-#include <QJsonDocument>
-#include <QJsonObject>
 #include <QObject>
+#include <QJsonDocument>
+#include<iostream>
+
 #include "QX-800_Model/Img_detect.h"
 #include "QX-800_Model/daheng_camera.h"
+#include "QX-800_Model/read_json.h"
+#include "QX-800_Model/receive_signal.h"
+#include "QX-800_View/MainWindowView/MainWindowView.h"
 
-class receive : public QObject
-{    Q_OBJECT
-private:
-    cv::Mat result;
-    std::vector<cv::Rect> m_ROI;
-    cv::Mat m_img;
-    bool *SavepictutePtr;
-
-    chip_detect *m_Detect;
-    DaHeng_camera *m_Picutre;
-
+class Controller : public QObject
+{
+    Q_OBJECT
 public:
-    explicit receive(QObject *parent = nullptr);// 默认构造函数
-
-public slots:
-    void slotReciveParamete(std::vector<int> &Para);         // 接收算法参数
-    void slotReciveFile(QString, QString);                   // 接收图片路径和ROI.Json，用于离线测试
-    void slotWriteJson(QJsonObject JsonObj);                 // 接收修改信息并写入json文件
-    void slotReceiveROIJson(std::vector<cv::Rect> roi_data); // 接收ROI.Json信号用于算法检测
-    void slotReceiveTakePhoto(cv::Mat img);                  // 接收拍照图片用于算法检测
-    void slotReceiveSavePicture(cv::Mat img);                // 接收拍照图片并保存
+    explicit Controller(QObject *parent = nullptr);
+    void modify_json();
 
 signals:
-    void signal_sent_detect_Image(QImage img);                    // 发送检测后的图片(离线测试)
-    void signal_sentROI_ToDetect(std::vector<cv::Rect> roi_data); // 发送ROI区域信息到后端检测
-    // void signal_sentCameraData_toMainWindow(QJsonObject JsonObj); // 发送曝光时间
-    void signal_sent_Detect_Img(QImage);                          // 发送检测后的图片（算法检测）
+
+private:
+    MainWindowView* m_MainWindowView = nullptr;
+    //MainWindowModel* m_MainWindowModel = nullptr;
+
+    receive* m_A;
+    DaHeng_camera* m_C;
+    read_json *m_Json;
+    chip_detect *m_I;
 };
 
-#endif // RECEIVE_SIGNAL_H
+#endif // CONTROLLER_H
