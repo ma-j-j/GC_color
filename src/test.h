5dfe001cdd8eea426a4cb7f218221bfe1cdf968e
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-07-17 15:33:03 +0800

    Update test.h

--- src/test.h
+++ src/test.h
@@ -1,85 +1,69 @@
-#ifndef RECTANGULARCALIBRATIONGRAPHICSVIEW_H
-#define RECTANGULARCALIBRATIONGRAPHICSVIEW_H
+#ifndef QLOG4CPLUS_H
+#define QLOG4CPLUS_H
 
 #include <QObject>
-#include <QGraphicsView>
-#include <QGraphicsScene>
-#include <QGraphicsPixmapItem>
-#include <QWheelEvent>
-#include <QMouseEvent>
-#include <QMenu>
+#include <QDateTime>
+#include <QDir>
+#include <mutex>
 #include <QDebug>
-#include <QAction>
-#include <QList>
-#include <QGraphicsPolygonItem>
 
-#include "opencv2/opencv.hpp"
+//日志
+#include "log4cplus/log4cplus.h"
+#include "log4cplus/fileappender.h"
+#include "log4cplus/layout.h"
 
-// QX-800矩形标定视图模块
-class RectangularCalibrationGraphicsview : public QGraphicsView
+//宏定义日志，减少代码量
+#define My_LOG(level, message) QLog4cplus::getQLog4cplusInstance().writeLog(level, message)
+#define My_OLog(level, message) QLog4cplus::getQLog4cplusInstance().onlyWriteLog(level, message)
+
+// 宏定义日志等级
+#define QLOG_DEBUG QLog4cplus::l_DEBUG
+#define QLOG_INFO QLog4cplus::l_INFO
+#define QLOG_WARN QLog4cplus::l_WARN
+#define QLOG_ERROR QLog4cplus::l_ERROR
+
+class QLog4cplus : public QObject
 {
     Q_OBJECT
 public:
-    RectangularCalibrationGraphicsview(QWidget *parent = nullptr);
-    ~RectangularCalibrationGraphicsview();
-public:
-    // 根据图像路径加载图像到场景
-    void loadFileImage(const QString &imagePath);
-    // 直接传递QPixmap图像加载到场景
-    void loadQPixmapImage(const QPixmap& pixmap);
-    // 直接传递CV::Mat图像加载到场景
-    void loadMatImage(const cv::Mat& srcImg);
-    // 获取加载图像的尺寸
-    QSize getImageSize();
-    // 更新图像缩放
-    void updateImageScale();
-    // 场景清除
-    void clear();
-public slots:
-    // 接收图像路径加载图像到场景
-    void slotLoadFileImage(const QString &imagePath);
-    // 直接传递QPixmap图像加载到场景
-    void slotLoadQPixmapImage(const QPixmap& pixmap);
-    // 直接传递CV::Mat图像加载到场景
-    void slotLoadMatImage(const cv::Mat& srcImg);
-    // 接收自动生成的标定区域
-    void slotReciveQRectFRoi(QVector<QRectF>);
-signals:
-    // 发送绘制完成的标定ROI信息
-    void signalSendROI(QRectF);
-    void signalSend_RectangleInformation(cv::Rect);
-
-protected:
-    // 将cv::Mat 转换为QPixmap
-    QPixmap matToQPixmap(const cv::Mat& matImg);
-protected:
-    // 鼠标点击事件
-    void mousePressEvent(QMouseEvent *event) override;
-    // 鼠标按下移动
-    void mouseMoveEvent(QMouseEvent *event) override;
-    // 鼠标释放事件
-    void mouseReleaseEvent(QMouseEvent *event) override;
+    enum Level {l_DEBUG=1,l_INFO,l_WARN,l_ERROR};
 private:
-    // 场景
-    QGraphicsScene* m_Scene = nullptr;
-    // 源图像
-    QPixmap m_Pixmap;
-    // 起始坐标和结束坐标
-    QPointF m_StartPoint,m_EndPoint;
-    // 正在绘制标识符
-    bool m_FRectDrawing;
-    // 拖拽模式标识符
-    bool m_FRectDragging;
-    // 存储拖拽原始点击位置
-    QPointF m_DraggingStartPoint;
-    // 矩形图元
-    QGraphicsRectItem* m_CurrentRect = nullptr;
-    // 存储矩形区域
-    QRectF m_RectF;
-    // 存储矩形区域
-    cv::Rect m_Rect;
-    // 存储缩放比例
-    qreal m_Scale;
+    //私有构造函数
+    QLog4cplus();
+public:
+    //删除拷贝构造和赋值构造
+    QLog4cplus(const QLog4cplus&) = delete;
+    QLog4cplus& operator=(const QLog4cplus&) = delete;
+    //提供一个公共的静态方法来获取实例
+    static QLog4cplus& getQLog4cplusInstance();
+    //析构函数
+    ~QLog4cplus();
+    //设置底层日志等级
+    void setQlogLevel(QLog4cplus::Level level);
+    //设置同步到界面的日志等级
+    void setQUIlogLevel(QLog4cplus::Level level);
+    //写入日志并且同步界面日志
+    void writeLog(QLog4cplus::Level,const char* message);
+    //只写入日志
+    void onlyWriteLog(QLog4cplus::Level,const char* message);
+protected:
+    //日志等级转QString
+    QString levelToString(QLog4cplus::Level level);
+signals:
+    void signalLogOutput(QLog4cplus::Level,QString);
+private:
+    //log4cplus日志对象
+    log4cplus::Logger m_logger;
+    //饿汉式的私有静态实例
+    static QLog4cplus instance;
+    //日志写入日志并且同步界面日志互斥锁
+    std::mutex m_mtx;
+    //日志只写入日志互斥锁
+    std::mutex m_mtxOnly;
+    //底层日志等级
+    int m_logLevel = 2;
+    //是否同步到界面日志等级
+    int m_UIlogLevel = 1;
 };
 
-#endif // RECTANGULARCALIBRATIONGRAPHICSVIEW_H
+#endif // QLOG4CPLUS_H
