433d8e8114797c1da85570c25b935350f4d576f6
ma-j-j <157667652+ma-j-j@users.noreply.github.com>
2025-08-08 11:43:56 +0800

    Update test.h

--- src/test.h
+++ src/test.h
@@ -1,63 +1,115 @@
-#ifndef ALGORITHMPARAMETERVIEW_H
-#define ALGORITHMPARAMETERVIEW_H
+#ifndef CAMERA_H
+#define CAMERA_H
 
+#include <QDebug>
 #include <QJsonObject>
 #include <QMessageBox>
 #include <QObject>
-#include <QObjectList>
-#include <QSpinBox>
-#include <QWidget>
 #include "15000_BackEnd/log4cplus/qlog4cplus.h"
+#include <GalaxyIncludes.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
-namespace Ui {
-class AlgorithmParameterView;
+// 图片标志位
+enum class PictureType {
+    img_save,                      // 模式类型0
+    img_show,                      // 模式类型1
+    img_detect_LoaderMode_Before,  // 模式类型2
+    img_detect_LoaderMode_Middle,  // 模式类型3
+    img_detect_LoaderMode_After,   // 模式类型4
+    img_detect_CuttingMode_Before, // 模式类型5
+    img_detect_CuttingMode_Middle, // 模式类型6
+    img_detect_CuttingMode_After   // 模式类型7
+};
+
+// 定义字符串到 PictureType 的映射
+static QMap<QString, PictureType> createPictureTypeMapping()
+{
+    QMap<QString, PictureType> mapping;
+    mapping["OffLineImageSave"] = PictureType::img_save;
+    mapping["OffLineImageShow"] = PictureType::img_show;
+    mapping["LoaderMode_Before"] = PictureType::img_detect_LoaderMode_Before;
+    mapping["LoaderMode_Middle"] = PictureType::img_detect_LoaderMode_Middle;
+    mapping["LoaderMode_After"] = PictureType::img_detect_LoaderMode_After;
+    mapping["CuttingMode_Before"] = PictureType::img_detect_CuttingMode_Before;
+    mapping["CuttingMode_Middle"] = PictureType::img_detect_CuttingMode_Middle;
+    mapping["CuttingMode_After"] = PictureType::img_detect_CuttingMode_After;
+    return mapping;
 }
 
-// 每个板型对应一个 参数名 -> 控件指针 的映射表
-struct BoardControlMap
+// 用于检测模式的全局映射表
+static const QMap<QString, PictureType> g_pictureTypeMapping = createPictureTypeMapping();
+
+// 相机拍照信息
+struct CameraImage_information
 {
-    QWidget *page;                            // 当前板型对应的 stackedWidget 页面
-    std::map<QString, QSpinBox *> spinBoxMap; // 参数名 -> 对应的 spinBox 控件
+    int CamerID;
+    PictureType m_PictureType;
+    std::string CameraSN;
+    cv::Mat img;
 };
 
-class AlgorithmParameterView : public QWidget
+// 用户继承采集事件处理类
+class CSampleCaptureEventHandler : public QObject, public ICaptureEventHandler
 {
     Q_OBJECT
+private:
+    std::shared_ptr<CameraImage_information> m_imgInformation;
 
 public:
-    explicit AlgorithmParameterView(QWidget *parent = nullptr);
-    ~AlgorithmParameterView();
-public slots:
-    // 保存参数按钮被点击
-    void slots_AlgorithmParameterView_On_PushButton_SaveParame_Clecked();
-    // 接收后端参数模型数据显示到界面
-    void slots_AlgorithmParameterView_Receive_AlgorithmParameterModel(std::map<QString, QJsonObject>,
-                                                                      QString);
-    // 接收选择的BIB板类型
-    void slots_AlgorithmParameterView_Receive_BIBBoard_TypeChanged(int);
+    explicit CSampleCaptureEventHandler(const std::string &sn, int camerID)
+    {
+        m_imgInformation->CameraSN = sn;
+        m_imgInformation->CamerID = camerID;
+    }
+
+    //回调函数采集图像
+    void DoOnImageCaptured(CImageDataPointer &objImageDataPointer, void *pUserParam);
+
+    // 相机用于设置图片模式
+    void setPictureType(PictureType type) { m_imgInformation->m_PictureType = type; }
 
 signals:
-    // 向后端发送待保存本地的参数并且让参数生效
-    void signals_AlgorithmParameterView_Sent_SaveParamJson(std::map<QString, QJsonObject>, QString);
-
-protected:
-    // 算法参数模块按钮初始化
-    void controlInit();
-    // 算法参数模块按钮样式初始化
-    void controlStyleInit();
-    // 控件QSpinBox初始化
-    void QSpinBoxInit();
-    // BoardType映射表
-    void BoardTypeInit();
-
-    std::map<QString, BoardControlMap> boardTypeToControls;
-
-private:
-    Ui::AlgorithmParameterView *ui;
-    // 通用样式设置函数
-    void applyStyleToWidget(QWidget *widget, const QString &qssStyle);
-    int m_BIBBoradTypeIndex;
-    QString m_BIBBoradTypeName;
+    void signals_Camer_Sent_TakePhoto(std::shared_ptr<CameraImage_information>); //发图片
 };
 
-#endif // ALGORITHMPARAMETERVIEW_H
+class Camera : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Camera(QObject *parent = nullptr);
+    ~Camera();
+
+    //初始化相机
+    void Camera_Init();
+
+    // 使用map来建立SN与编号的映射关系，更清晰易维护
+    std::unordered_map<std::string, int> m_CameraMapTable = {
+        {"LKR25070001", 0}, // 1号相机
+        {"LKR25070002", 1}, // 2号相机
+        {"LKR25070003", 2}  // 3号相机
+    };
+
+public slots:
+    void slots_Camera_Receive_Exposure(QJsonObject);   // 接收曝光时间
+    void slots_Camera_Receive_TakePhoto_Mode(QString); // 接收检测模式
+
+signals:
+    void signals_Camera_Sent_SavePicture(std::shared_ptr<CameraImage_information>); // 发图片用于保存
+    void singals_Camera_Sent_ShowPicture(
+        std::shared_ptr<CameraImage_information>); // 发图片用于离线检测显示
+    void signals_Camera_Sent_DetectThread(
+        std::shared_ptr<CameraImage_information>); // 发图片用于算法检测
+
+private:
+    CSampleCaptureEventHandler *m_CSampleCapture;
+    ICaptureEventHandler *m_ICapture;
+    // 相机的成员变量
+    std::vector<CGXDevicePointer> m_devices;
+    std::vector<CGXStreamPointer> m_streams;
+    std::vector<CGXFeatureControlPointer> m_featureControls;
+    std::vector<CSampleCaptureEventHandler *> m_eventHandlers;
+    int m_Camera_Exposure; // 相机曝光时间
+};
+
+#endif // CAMERA_H
